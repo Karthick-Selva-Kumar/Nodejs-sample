4b1d3889ca0bf07c035332f975a740ed7f7156e2
Ubuntu <ubuntu@ip-172-31-10-18.ap-south-1.compute.internal>
2024-01-03 14:32:02 +0000

    Node-app

---
+++ app.js
@@ -0,0 +1,21 @@
+// Importing the http module 
+const http = require("http");
+
+// Defining the port
+const port = 3000;
+
+// Creating server  
+const server = http.createServer((req, res) => {
+    // Sending the response 
+    res.write("Hello World");
+    res.end();
+});
+
+// Server listening to port 3000 
+server.listen(port, function () {
+    // Logging to console
+    console.log(`Todolist running on http://0.0.0.0:${port}`);
+});
+
+
+
